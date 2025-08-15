513558531e0dee8ae9aea868a30321017cd3cb34
zaynbsn <nils.vainui@gmail.com>
2025-08-15 12:07:25 +0200

    update formations

--- app/components/about/Formations.vue
+++ app/components/about/Formations.vue
@@ -2,12 +2,55 @@
   <section class="max-w-5xl mx-auto px-4 text-[#61848d]">
     <h2 class="text-3xl md:text-4xl font-bold mb-8">Mes formations continues</h2>
 
-    <ul class="list-disc list-inside text-base space-y-1">
-      <li>2023 - 3ème colloque de l’orthopédagogie à Nice</li>
-      <li>2023 - Formation en ligne TDA/H - Association HypersSupers TDA/H France</li>
-      <li>2024 - Gestions des écrans - journée thématique Sébastien Henrard</li>
-      <li>2024 - Conférence en ligne de Guy Sonnois . Retrouver le sens au coeur des apprentissages</li>
-      <li>2024 - La motivation - Cédrine Sunyer</li>
+    <ul class="list-disc text-base space-y-1">
+      <li>
+        2019
+        <ul class="list-disc list-inside text-base space-y-1">
+          <li>MOOC L’attention ça s'apprend. JPh Lachaux</li>
+        </ul>
+      </li>
+
+      <li>
+        2021
+        <ul class="list-disc list-inside text-base space-y-1">
+          <li>Parcours Approfondissement en Orthopédagogie</li>
+        </ul>
+      </li>
+
+      <li>
+        2022
+        <ul class="list-disc list-inside text-base space-y-1">
+          <li>Motivation et projet de sens. Initiative & Formation Paris</li>
+        </ul>
+      </li>
+      
+      <li>
+        2023
+        <ul class="list-disc list-inside text-base space-y-1">
+          <li>3ème colloque de l’orthopédagogie à Nice</li>
+          <li>Formation en ligne TDA/H - Association HypersSupers TDA/H France</li>
+          <li>MOOC Apprendre comment apprendre (Univ San Diego)</li>
+        </ul>
+      </li>
+      
+      <li>
+        2024
+        <ul class="list-disc list-inside text-base space-y-1">
+          <li>Formation à la Gestions des écrans - journée thématique Sébastien Henrard</li>
+          <li>Conférence en ligne de Guy Sonnois . Retrouver le sens au coeur des apprentissages</li>
+          <li>Formation La motivation - Cédrine Sunyer</li>
+          <li>Formation La stimulation cognitive- Niveau Avancé - La Fabrique à Neurones</li>
+          <li>Formation TND Jérome Bianchi</li>
+        </ul>
+      </li>
+
+      <li>
+        2023
+        <ul class="list-disc list-inside text-base space-y-1">
+          <li>Webinaire Fonctions exécutives et apprentissages</li>
+          <li>Conférence : L’ouspsologie Pierre Paul Gagné</li>
+        </ul>
+      </li>
     </ul>
   </section>
 </template>
