7af34889dfc2aae3d0908d536d60238d5516a30c
zaynbsn <nils.vainui@gmail.com>
2025-08-26 16:19:13 +0200

    font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  css: ['~/assets/css/main.css', '~/assets/css/fonts.css'],
   vite: {
     plugins: [
       tailwindcss(),
