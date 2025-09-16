7c5839f271df28afcb042ce356fcf5f1a5354ca8
zaynbsn <nils.vainui@gmail.com>
2025-09-16 18:31:53 +0200

    timeline formations

--- app/components/about/Formations.vue
+++ app/components/about/Formations.vue
@@ -3,36 +3,15 @@
     <h2 class="text-3xl md:text-4xl font-bold mb-8">Mes formations continues</h2>
 
     <ul class="list-disc text-base space-y-1">
-      <li>
-        2019
-        <ul class="list-disc list-inside text-base space-y-1">
-          <li>MOOC L’attention ça s'apprend. JPh Lachaux</li>
-        </ul>
-      </li>
-
-      <li>
-        2021
-        <ul class="list-disc list-inside text-base space-y-1">
-          <li>Parcours Approfondissement en Orthopédagogie</li>
-        </ul>
-      </li>
-
-      <li>
-        2022
-        <ul class="list-disc list-inside text-base space-y-1">
-          <li>Motivation et projet de sens. Initiative & Formation Paris</li>
-        </ul>
-      </li>
       
       <li>
         2023
         <ul class="list-disc list-inside text-base space-y-1">
-          <li>3ème colloque de l’orthopédagogie à Nice</li>
-          <li>Formation en ligne TDA/H - Association HypersSupers TDA/H France</li>
-          <li>MOOC Apprendre comment apprendre (Univ San Diego)</li>
+          <li>Webinaire Fonctions exécutives et apprentissages</li>
+          <li>Conférence : L’ouspsologie Pierre Paul Gagné</li>
         </ul>
       </li>
-      
+
       <li>
         2024
         <ul class="list-disc list-inside text-base space-y-1">
@@ -44,11 +23,36 @@
         </ul>
       </li>
 
+      
+      
       <li>
         2023
         <ul class="list-disc list-inside text-base space-y-1">
-          <li>Webinaire Fonctions exécutives et apprentissages</li>
-          <li>Conférence : L’ouspsologie Pierre Paul Gagné</li>
+          <li>3ème colloque de l’orthopédagogie à Nice</li>
+          <li>Formation en ligne TDA/H - Association HypersSupers TDA/H France</li>
+          <li>MOOC Apprendre comment apprendre (Univ San Diego)</li>
+        </ul>
+      </li>
+      
+      <li>
+        2022
+        <ul class="list-disc list-inside text-base space-y-1">
+          <li>Motivation et projet de sens. Initiative & Formation Paris</li>
+        </ul>
+      </li>
+
+      <li>
+        2021
+        <ul class="list-disc list-inside text-base space-y-1">
+          <li>Parcours Approfondissement en Orthopédagogie</li>
+        </ul>
+      </li>
+
+
+      <li>
+        2019
+        <ul class="list-disc list-inside text-base space-y-1">
+          <li>MOOC L’attention ça s'apprend. JPh Lachaux</li>
         </ul>
       </li>
     </ul>
