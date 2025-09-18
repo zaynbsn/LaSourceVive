b3bd424a05052680a44e3e7da03f187d870b0386
zaynbsn <nils.vainui@gmail.com>
2025-09-18 20:07:38 +0200

    SEO nuxt sitemap + head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,16 +5,37 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css', '~/assets/css/fonts.css'],
+
+  site: { 
+    url: 'https://orthopedagogie-annecy.fr', 
+    name: 'Julie Boisson - Orthopédagogie - Annecy' 
+  }, 
+
+  app: {
+    head: {
+      title: 'Julie Boisson - Orthopédagogie - Annecy',
+      meta: [
+        { name: 'description', content: 'Orthopédagogie à Annecy – Julie Boisson, orthopédagogue certifiée, propose un accompagnement personnalisé pour enfants, adolescents et adultes présentant des troubles ou difficultés d’apprentissage. Retrouvez confiance, motivation et autonomie grâce à une écoute bienveillante et des interventions adaptées à chaque profil.' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      ]
+    }
+  },
+
   vite: {
     plugins: [
       tailwindcss(),
     ],
   },
+
   runtimeConfig: {
     public: {
       emailServiceId: process.env.EMAIL_SERVICE_ID,
       emailTemplateId: process.env.EMAIL_TEMPLATE_ID,
       emailApiKey: process.env.EMAIL_API_KEY
     }
-  }
+  },
+
+  modules: ['@nuxtjs/sitemap']
 })
